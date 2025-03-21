1e665c5a6bcff584641b7c05802b8d682895b7fc
Marcus Lim <markerlim15@gmail.com>
2025-03-21 09:25:55 +0800

    done task 1.2 haven do user

---
+++ data/restaurant.sql
@@ -0,0 +1,30 @@
+-- IMPORTANT: DO NOT CHANGE THE GIVEN SCHEMA UNLESS YOU HAVE A GOOD REASON
+-- IF YOU DO CHANGE IT WRITE THE JUSTIFICATION IN A COMMENT ABOVE THE CHANGE
+
+drop database if exists restaurant;
+
+create database restaurant;
+
+use restaurant;
+
+create table customers (
+  username varchar(64) not null,
+  password varchar(128) not null
+);
+
+insert into customers(username, password) values
+  ('fred', sha2('fred', 224)),
+  ('barney', sha2('barney', 224)),
+  ('wilma', sha2('wilma', 224)),
+  ('betty', sha2('betty', 224)),
+  ('pebbles', sha2('pebbles', 224));
+
+-- TODO: Task 1.2
+-- Write your task 1.2 below
+create table place_orders(
+  `order_id` char(8) not null,
+  `payment_id` varchar(128) not null,
+  `order_date` date not null,
+  `total` float not null check(total <= 999999.99),
+  `username` varchar(64) not null
+)
